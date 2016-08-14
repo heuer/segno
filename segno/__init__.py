307f37441281a93bc27df08203b2c75601165aaa
Lars <heuer@semagia.com>
2016-08-14 22:20:06 +0200

    Updated CHANGES / version no

--- segno/__init__.py
+++ segno/__init__.py
@@ -22,7 +22,7 @@ from .encoder import QRCodeError, ErrorLevelError, ModeError, MaskError, \
     VersionError, DataOverflowError
 from . import writers, utils
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ('make', 'make_qr', 'make_micro', 'QRCode', 'QRCodeError',
            'ErrorLevelError', 'ModeError', 'MaskError', 'VersionError',
