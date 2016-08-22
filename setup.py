10d9d85d7dff1b61564bd61b80b8fbc6ea063b83
Lars <heuer@semagia.com>
2016-08-22 22:07:53 +0200

    Updated keywords for PyPI

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
     include_package_data=True,
     entry_points = {'console_scripts': ['segno = segno.scripts.cmd:main']},
     keywords=['QR Code', 'Micro QR Code', 'ISO/IEC 18004',
-              'ISO/IEC 18004:2006(E)', 'ISO/IEC 18004:2015(E)'],
+              'ISO/IEC 18004:2006(E)', 'ISO/IEC 18004:2015(E)', 'qrcode', 'QR',
+              'barcode', 'matrix', '2D'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
