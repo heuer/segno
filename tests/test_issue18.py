72a3d4f6ad6a8301c3a67010d62f8bf00b4b79e1
Lars <heuer@semagia.com>
2016-09-02 00:04:44 +0200

    Updated issue #18 test cases

--- tests/test_issue18.py
+++ tests/test_issue18.py
@@ -38,6 +38,28 @@ def test_issue_18_automatic():
     assert 'M' == qr.error
 
 
+def test_issue_18_zero():
+    qr = segno.make_qr(0)
+    assert 1 == qr.version
+    assert 'numeric' == qr.mode
+    assert 'H' == qr.error
+
+
+def test_issue_18_zero_micro():
+    qr = segno.make_micro(0)
+    assert 'M1' == qr.version
+    assert 'numeric' == qr.mode
+    assert qr.error is None
+
+
+def test_issue_18_zero_automatic():
+    qr = segno.make(0)
+    assert 'M1' == qr.version
+    assert 'numeric' == qr.mode
+    assert qr.error is None
+
+
+
 if __name__ == '__main__':
     import pytest
     pytest.main([__file__])
