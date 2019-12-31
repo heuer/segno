fc9581cd8a83bd2f368963aa94ded671cb768556
Lars <heuer@semagia.com>
2019-12-31 15:37:21 +0100

    Added test case

--- tests/test_png_colormap.py
+++ tests/test_png_colormap.py
@@ -13,6 +13,7 @@ import io
 import pytest
 import segno
 from segno import moduletypes as mt
+from segno import colors
 from png import Reader as PNGReader
 
 
@@ -57,6 +58,7 @@ def test_plte():
     dark_with_alpha = dark + (255,)
     assert dark_with_alpha in palette
 
+
 def test_plte_no_transparency():
     qr = segno.make_qr('test')
     buff = io.BytesIO()
@@ -72,6 +74,29 @@ def test_plte_no_transparency():
     assert 2 == len(palette)
     assert dark in palette
     assert light in palette
+
+
+def test_plte_colors():
+    qr = segno.make_qr('test')
+    buff = io.BytesIO()
+    dark = (0, 0, 139)
+    light = (255, 255, 255)
+    colormap = _make_colormapping(dark=dark, light=light)
+    colormap[mt.TYPE_QUIET_ZONE] = 'green'
+    colormap[mt.TYPE_FINDER_PATTERN_DARK] = 'purple'
+    colormap[mt.TYPE_FINDER_PATTERN_LIGHT] = 'yellow'
+    qr.save(buff, kind='png', colormap=colormap)
+    buff.seek(0)
+    reader = PNGReader(file=buff)
+    reader.preamble()
+    assert not reader.greyscale
+    palette = reader.palette()
+    assert 5 == len(palette)
+    assert dark in palette
+    assert light in palette
+    assert colors.color_to_rgb('green') in palette
+    assert colors.color_to_rgb('purple') in palette
+    assert colors.color_to_rgb('yellow') in palette
 
 
 def _make_colormapping(dark, light):
