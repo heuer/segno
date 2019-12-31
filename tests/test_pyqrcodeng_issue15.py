38b458fee0a3b33b56673fa238c77299bf1d162c
Lars <heuer@semagia.com>
2019-12-31 23:03:26 +0100

    Updated test cases

--- tests/test_pyqrcodeng_issue15.py
+++ tests/test_pyqrcodeng_issue15.py
@@ -24,11 +24,13 @@ def test_version_and_error_provided():
     # QR Code version 1-L: Max. 25 alphanumeric chars
     qr = segno.make('A' * 25, version=1, error='l')
     assert '1-L' == qr.designator
+    assert 'alphanumeric' == qr.mode
 
 
 def test_version_and_error_provided2():
     qr = segno.make('A' * 25, version=1)  # No error level defined
     assert '1-L' == qr.designator
+    assert 'alphanumeric' == qr.mode
 
 
 if __name__ == '__main__':
