22403ae6f903a5342264f2b82ff74e4b0e7cc2bf
Lars <heuer@semagia.com>
2020-01-08 10:06:46 +0100

    Updated test cases, see #62

--- tests/test_png_plte.py
+++ tests/test_png_plte.py
@@ -7,6 +7,8 @@
 #
 """\
 Tests if the PNG serializer does not add more colors than needed.
+
+See also issue <https://github.com/heuer/segno/issues/62>
 """
 from __future__ import unicode_literals, absolute_import
 import io
@@ -16,30 +18,33 @@ import segno
 
 def test_plte():
     qr = segno.make_qr('test')
+    assert qr.version < 7
     dark = 'red'
     buff_1 = io.BytesIO()
     buff_2 = io.BytesIO()
-    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark)
+    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark, version_dark='green')
     qr.save(buff_2, kind='png', dark=dark)
     assert buff_1.getvalue() == buff_2.getvalue()
 
 
 def test_plte2():
     qr = segno.make_qr('test')
+    assert qr.version < 7
     dark = 'red'
     buff_1 = io.BytesIO()
     buff_2 = io.BytesIO()
-    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark)
+    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark, version_dark='green')
     qr.save(buff_2, kind='png', dark=dark)
     assert buff_1.getvalue() == buff_2.getvalue()
 
 
 def test_plte3():
     qr = segno.make_qr('test')
+    assert qr.version < 7
     dark = 'red'
     buff_1 = io.BytesIO()
     buff_2 = io.BytesIO()
-    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark, dark_module=dark)
+    qr.save(buff_1, kind='png', dark=dark, finder_dark=dark, version_dark='green')
     qr.save(buff_2, kind='png', dark=dark)
     assert buff_1.getvalue() == buff_2.getvalue()
 
