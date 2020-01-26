5195148ee433824368b9d6fd3e4eb0daa4bb2029
Lars <heuer@semagia.com>
2020-01-26 22:23:10 +0100

    Removed unused import, see issue #66

--- segno/__init__.py
+++ segno/__init__.py
@@ -13,7 +13,6 @@ QR Code and Micro QR Code implementation.
 from __future__ import absolute_import, unicode_literals
 import sys
 from . import encoder
-import warnings
 from . import writers, utils
 try:  # pragma: no cover
     str_type = basestring
