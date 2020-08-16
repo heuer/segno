15af78639a73cf51bf0089388255125f294f1818
Lars <heuer@semagia.com>
2020-08-16 12:05:22 +0200

    Added bechmarks to nox, added some sanity checks to release procedure

--- noxfile.py
+++ noxfile.py
@@ -10,6 +10,7 @@ Nox test runner configuration.
 """
 import os
 import sys
+import re
 from functools import partial
 from itertools import chain
 import shutil
@@ -79,13 +80,26 @@ def lint(session):
     session.run('flake8', 'segno')
 
 
+@nox.session(python=default_py)
+def benchmarks(session):
+    """\
+    Run the benchmarks and create the charts.
+    """
+    session.install('qrcode', 'Pillow', 'pyqrcode', 'pypng', 'pyqrcodeng', 'qrcodegen', 'pygal')
+    session.install('.')
+    sandbox_dir = os.path.join(os.path.dirname(__file__), 'sandbox')
+    session.run('python', os.path.join(sandbox_dir, 'benchmarks.py'))
+    session.run('python', os.path.join(sandbox_dir, 'make_charts.py'))
+
+
 #
 # Release related tasks
 # 1. nox -e start-release -- version-number
 # 2. run tests, update docs, update changes
 # 3. nox -e finish-release -- version-number
 # 4. git push / git push origin --tags
-# 5. nox -e upload-release -- version-number
+# 5. nox -e build-release
+# 6. nox -e upload-release
 #
 
 
@@ -97,13 +111,24 @@ def start_release(session):
     * Creates a new branch release-VERSION_NUMBER
     * Changes the version number in segno.__version__ to VERSION_NUMBER
     """
+    session.install('packaging')
+    git = partial(session.run, 'git', external=True)
+    git('checkout', 'develop')
+    prev_version = _get_current_version(session)
     version = _validate_version(session)
+    valid_version = bool(int(session.run('python', '-c', 'from packaging.version import parse;'
+                                                         'prev_version = parse("{0}");'
+                                                         'next_version = parse("{1}");'
+                                                         'print(1 if prev_version < next_version else 0)'
+                                         .format(prev_version, version), silent=True)))
+    if not valid_version:
+        session.error('Invalid version')
     release_branch = 'release-{}'.format(version)
-    session.run('git', 'checkout', '-b', release_branch, 'develop', external=True)
-    init_py = os.path.abspath(os.path.join(os.path.dirname(__file__), 'segno/__init__.py'))
-    session.run('sed', '-i', "s/__version__ = '{0}.dev.'/__version__ = '{0}'/".format(version), init_py, external=True)
+    git('checkout', '-b', release_branch, 'develop')
+    _change_version(session, prev_version, version)
+    git('add', 'segno/__init__.py')
     session.log('Now on branch "{}". Run the tests, run nox -e docs. Update CHANGES'.format(release_branch))
-    session.log('When done, call nox -e finish-release')
+    session.log('When done, call nox -e finish-release -- {}'.format(version))
 
 
 @nox.session(name='finish-release', python=default_py)
@@ -118,7 +143,6 @@ def finish_release(session):
     """
     version = _validate_version(session)
     release_branch = 'release-{}'.format(version)
-    session.install('setuptools', 'wheel')
     git = partial(session.run, 'git', external=True)
     git('checkout', 'master')
     git('merge', '--no-ff', release_branch, '-m', 'Merge release branch {}'.format(release_branch))
@@ -129,13 +153,21 @@ def finish_release(session):
     version_parts = version.split('.')
     patch = str(int(version_parts[2]) + 1)
     next_version = '.'.join(chain(version_parts[:2], patch)) + '.dev'
-    init_py = os.path.abspath(os.path.join(os.path.dirname(__file__), 'segno/__init__.py'))
-    session.run('sed', '-i', "s/__version__ = '{0}'/__version__ = '{1}'/".format(version, next_version), init_py, external=True)
+    _change_version(session, version, next_version)
     git('add', 'segno/__init__.py')
     git('commit', '-m', 'Incremented development version')
-    git('checkout', 'master')
+    session.log('Finished. Run git push / git push origin --tags and nox -e build-release / nox -e upload-release')
+
+
+@nox.session(name='build-release', python=default_py)
+def build_release(session):
+    """\
+    Builds a release: Creates sdist and wheel
+    """
+    session.install('setuptools', 'wheel')
+    shutil.rmtree('dist', ignore_errors=True)
+    session.run('git', 'checkout', 'master', external=True)
     session.run('python', 'setup.py', 'sdist', 'bdist_wheel')
-    session.log('Finished. Run git push / git push origin --tags and nox -e upload-release')
 
 
 @nox.session(name='upload-release', python=default_py)
@@ -143,7 +175,6 @@ def upload_release(session):
     """\
     Uploads a release to PyPI
     """
-    version = _validate_version(session)
     session.install('twine')
     twine = partial(session.run, 'twine')
     twine('check', 'dist/*')
@@ -155,4 +186,36 @@ def _validate_version(session):
         session.error('No release version provided')
     elif len(session.posargs) > 1:
         session.error('Too many arguments')
-    return session.posargs[0]
+    version = session.posargs[0]
+    if not re.match(r'^[0-9]+\.[0-9]+\.[0-9]+$', version):
+        session.error('Invalid version number: "{}"'.format(version))
+    return version
+
+
+def _get_current_version(session):
+    """\
+    Returns the current Segno version.
+    """
+    fn = os.path.abspath(os.path.join(os.path.dirname(__file__), 'segno/__init__.py'))
+    with open(fn, 'r', encoding='utf-8') as f:
+        content = f.read()
+    m = re.search(r'^__version__ = ["\']([^"\']+)["\']$', content, flags=re.MULTILINE)
+    if m:
+        return m.group(1)
+    session.error('Cannot find any version information')
+
+
+def _change_version(session, previous_version, next_version):
+    """\
+    Changes the segno.__init__.__version__ from previous_version to next_version.
+    """
+    fn = os.path.abspath(os.path.join(os.path.dirname(__file__), 'segno/__init__.py'))
+    with open(fn, 'r', encoding='utf-8') as f:
+        content = f.read()
+    new_content = re.sub(r'^(__version__ = ["\'])({0})(["\'])$'.format(re.escape(previous_version)),
+                         r'\g<1>{}\g<3>'.format(next_version), content, flags=re.MULTILINE)
+    if content != new_content:
+        with open(fn, 'w', encoding='utf-8') as f:
+            f.write(new_content)
+        return
+    session.error('Cannot modify version. Provided: "{}" (previous) "{}" (next)'.format(previous_version, next_version))
